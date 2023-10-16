2469cdadc534e82eaf12cd57d7ed813afdbcfa95
thellzardi12 <130668477+thellzardi12@users.noreply.github.com>
2023-10-16 16:40:57 -0300

    - Commit Rebecca

--- aula 14 desafio 1.py
+++ aula 14 desafio 1.py
@@ -12,7 +12,16 @@ c=int (input('Informe o lado c:'))
 #Processamento e saída
 if a < (b+c) and b < (a+c) and c < (a+b):
     print(f'\n\n{a}, {b} e {c} formam um triângulo!')
+
+    #Tipo do Triangulo
+    if ( a==b) and ( b==c):
+        print('Equilátero')
+    if (a==b) or (a==c) or (b==c):
+        print('Isósceles')
+    if (a != b) and (a != c) and (b != c):
+        print('Escaleno')
 else:
     print(f'\n\n{a}, {b} e {c} não formam um triângulo!')
+
 
 
