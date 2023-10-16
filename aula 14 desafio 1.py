a1ac4d329d70adb525a87d1a7c2af9ded5b72bba
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-10-16 16:12:08 -0300

    -commit inicial

---
+++ aula 14 desafio 1.py
@@ -0,0 +1,18 @@
+#criar um validador de Triangulo
+
+#Apresentação
+
+print('\n\t\t\t ---Validador de Triângulo')
+
+#Entrada
+a=int (input('Informe o lado a:'))
+b=int (input('Informe o lado b:'))
+c=int (input('Informe o lado c:'))
+
+#Processamento e saída
+if a < (b+c) and b < (a+c) and c < (a+b):
+    print(f'\n\n{a}, {b} e {c} formam um triângulo!')
+else:
+    print(f'\n\n{a}, {b} e {c} não formam um triângulo!')
+
+
