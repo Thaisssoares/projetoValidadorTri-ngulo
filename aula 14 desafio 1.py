a59d626fd897452c7bc03d1128574eb039fdf677
thellzardi12 <130668477+thellzardi12@users.noreply.github.com>
2023-10-17 14:26:43 -0300

    - Commit Rebecca 2

--- aula 14 desafio 1.py
+++ aula 14 desafio 1.py
@@ -14,11 +14,11 @@ if a < (b+c) and b < (a+c) and c < (a+b):
     print(f'\n\n{a}, {b} e {c} formam um triângulo!')
 
     #Tipo do Triangulo
-    if ( a==b) and ( b==c):
+    if (a==b) and (b==c):
         print('Equilátero')
-    if (a==b) or (a==c) or (b==c):
+    elif (a==b) or (a==c) or (b==c):
         print('Isósceles')
-    if (a != b) and (a != c) and (b != c):
+    else:
         print('Escaleno')
 else:
     print(f'\n\n{a}, {b} e {c} não formam um triângulo!')
